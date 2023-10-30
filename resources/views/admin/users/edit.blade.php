2186c4badb5e962b7e1b805d39757d6b28065704
Hugo Pinheiro <pinheirosossahugo@gmail.com>
2023-10-30 15:42:16 -0400

    tabla_actualizacion2

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,41 +8,27 @@
 
 @section('content')
 
-    @if(session('info'))
+    @if (session('info'))
         <div class="alert alert-succes">
-            <strong>{{session('info')}}</strong>
+            <strong>{{ session('info') }}</strong>
         </div>
     @endif
-    
+
     <div class="card">
         <div class="card-body">
             <p class="h5">Nombre:</p>
             <p class="form-control">{{ $user->name }}</p>
 
 
-            <h2 class="h5">Listado de roles</h2>
-            {!! Form::model($user, ['route' => ['admin.users.update', $user], 'method' => 'put']) !!}
-            @foreach ($roles as $role)
-                <div>
-                    <label>
-                        {!! Form::checkbox('roles[]', $role->id, null, ['class' => 'mr-1']) !!}
-                        {{ $role->name }}
-                    </label>
-                </div>
-            @endforeach
-
-            {!! Form::submit('Asignar un rol', ['class' => 'btn btn-primary mt-2']) !!}
-            {!! Form::close() !!}
         </div>
-    </div>
-@stop
+    @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+    @section('css')
+        <link rel="stylesheet" href="/css/admin_custom.css">
+    @stop
 
-@section('js')
-    <script>
-        console.log('Hi!');
-    </script>
-@stop
+    @section('js')
+        <script>
+            console.log('Hi!');
+        </script>
+    @stop
